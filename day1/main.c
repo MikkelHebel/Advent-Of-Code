858350ba8b58330a9c6d50aefdc7f9c9945529d2
MikkelHebel <mikkelhebelv2@gmail.com>
2025-12-03 15:00:19 +0100

    My solution for day1

---
+++ day1/main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int shift(char chars[]);
+
+int main(int argc, char *argv[]){
+  int dial = 50;
+  int pwd = 0;
+
+  FILE *fptr;
+  fptr = fopen("input", "r");
+
+  char charsFromFile[10];
+
+  if (fptr != NULL) {
+    while(fgets(charsFromFile, 10, fptr)) {
+      dial += shift(charsFromFile);
+      if (dial % 100 == 0) {
+        pwd++;
+      }
+    }
+  }
+  printf("Done: %d\n", pwd);
+  return 0;
+}
+
+int shift(char chars[]) {
+  char ss[10];
+  int result = 0;
+  strncpy(ss, chars + 1, strlen(chars) - 2);
+  if (chars[0] == 'R') {
+    result = atoi(ss);
+  } else {
+    result = atoi(ss);
+    result *= (-1);
+  }
+  return result;
+}
