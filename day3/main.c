58524b9fb78bf1c6c1ea53c7b9f08d56af126560
MikkelHebel <mikkelhebelv2@gmail.com>
2025-12-11 21:17:16 +0100

    Completed day3

---
+++ day3/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+
+int findJoltage(char line[]);
+
+int main(int argc, char argv[]) {
+  FILE *fptr;
+  fptr = fopen("input", "r");
+
+  if (fptr == NULL) {
+    perror("Cannot open input");
+  }
+
+  char line[1024];
+  int totalJoltage = 0;
+  while(fgets(line, sizeof(line), fptr)) {
+    totalJoltage += findJoltage(line);
+  }
+  printf("Total Joltage: %d\n", totalJoltage);
+}
+
+int findJoltage(char line[]) {
+  int max_joltage = 0;
+  for (int i = 0; i < strlen(line); i++) {
+    int x = line[i] - '0';
+    for (int j = i + 1; j < strlen(line); j++) {
+      int y = line[j] - '0';
+      int temp = (10 * x) + y;
+      if (temp > max_joltage) {
+        max_joltage = (10 * x) + y;
+      }
+    }
+  }
+  return max_joltage;
+}
